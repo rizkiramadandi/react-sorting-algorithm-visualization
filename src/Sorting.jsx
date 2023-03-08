9210ffd739fd09bcafe595ac052aeb725f18848f
dandi16 <rramadandi16@gmail.com>
2023-03-08 14:21:30 +0700

    add dynamic array data count configuration

--- src/Sorting.jsx
+++ src/Sorting.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState, useRef } from "react"
 
-import { arrayRandomizer } from "./App";
-
 function delay(ms) {
     return new Promise(resolve => {
         setTimeout(() => {
@@ -10,7 +8,20 @@ function delay(ms) {
     });
 }
 
-export default function Sorting({ array, setArray }) {
+export function arrayRandomizer(length) {
+    let newArray = []
+    for(let i = 0 ; i < length ; i++) {
+      newArray.push({
+        isActive: false,
+        value: Math.floor(Math.random() * 9 ) + 1
+      })
+    }
+    return newArray
+  }
+
+export default function Sorting() {
+    const [count, setCount] = useState(10)
+    const [array, setArray] = useState([...arrayRandomizer(count)])
     const [max, setMax] = useState(null)
     const [sorting, setSorting] = useState(false)
     const delayValue = useRef(500)
@@ -18,6 +29,10 @@ export default function Sorting({ array, setArray }) {
     useEffect(() => {
         setMaxValue()
     }, [array])
+
+    useEffect(() => {
+        randomize()
+    }, [count])
 
     function setMaxValue() {
         setMax(prevState => {
@@ -29,13 +44,13 @@ export default function Sorting({ array, setArray }) {
         })
     }
 
-    function randomize(array) {
+    function randomize() {
         if(!sorting) {
-            setArray([...arrayRandomizer(array.length)])
+            setArray([...arrayRandomizer(count)])
         }
     }
 
-    async function bubbleSort(array) {
+    async function bubbleSort() {
         if(!sorting) {
             setSorting(true)
             for(let i = 0 ; i < array.length - 1 ; i++) {
@@ -66,7 +81,7 @@ export default function Sorting({ array, setArray }) {
         }
     }
 
-    async function selectionSort(array) {
+    async function selectionSort() {
         if(!sorting) {
             setSorting(true)
             for(let i = 0 ; i < array.length - 1 ; i++) {
@@ -100,7 +115,7 @@ export default function Sorting({ array, setArray }) {
         }
     }
 
-    async function insertionSort(array) {
+    async function insertionSort() {
         if(!sorting) {
             setSorting(true)
             for(let i = 1 ; i < array.length ; i++) {
@@ -150,37 +165,44 @@ export default function Sorting({ array, setArray }) {
 
     return (
         <>
-            <div className="flex h-[50vh] gap-1 items-end justify-center">
+            <div className="flex h-[50vh] gap-1 items-end w-fit mx-auto max-w-full overflow-x-auto">
                 { array.map((a, idx) => {
                     const customHeight = `${(a.value/max) * 100}%`
-                    const customWidth = `${100/array.length}%`
                     return (
-                        <div key={idx} className={`rounded p-1 text-center text-white font-bold flex items-end justify-center ${ a.isActive ? `bg-red-500`:`bg-cyan-500` }`} style={{ height: customHeight, width: customWidth }}>
+                        <div key={idx} className={`rounded px-2 text-center text-white font-bold flex items-end justify-center ${ a.isActive ? `bg-red-500`:`bg-cyan-500` }`} style={{ height: customHeight }}>
                             { a.value }
                         </div>
                     )
                 }) }
             </div>
+            
+            <div>
+                <div className="flex gap-1 justify-center flex-wrap mt-2">
+                    <button className={`bg-cyan-500 text-white rounded p-2 ${sorting ? `opacity-75 hover:cursor-not-allowed`:`hover:bg-cyan-600`}`} onClick={() => bubbleSort(array)}>
+                        Bubble Sort
+                    </button>
+                    <button className={`bg-cyan-500 text-white rounded p-2 ${sorting ? `opacity-75 hover:cursor-not-allowed`:`hover:bg-cyan-600`}`} onClick={() => selectionSort(array)}>
+                        Selection Sort
+                    </button>
+                    <button className={`bg-cyan-500 text-white rounded p-2 ${sorting ? `opacity-75 hover:cursor-not-allowed`:`hover:bg-cyan-600`}`} onClick={() => insertionSort(array)}>
+                        Insertion Sort
+                    </button>
+                    <button className={`bg-cyan-500 text-white rounded p-2 ${sorting ? `opacity-75 hover:cursor-not-allowed`:`hover:bg-cyan-600`}`} onClick={() => randomize(array)}>
+                        Randomize
+                    </button>
+                </div>
 
-            <div className="flex gap-1 justify-center flex-wrap mt-2">
-                <button className={`bg-cyan-500 text-white rounded p-2 ${sorting ? `opacity-75 hover:cursor-not-allowed`:`hover:bg-cyan-600`}`} onClick={() => bubbleSort(array)}>
-                    Bubble Sort
-                </button>
-                <button className={`bg-cyan-500 text-white rounded p-2 ${sorting ? `opacity-75 hover:cursor-not-allowed`:`hover:bg-cyan-600`}`} onClick={() => selectionSort(array)}>
-                    Selection Sort
-                </button>
-                <button className={`bg-cyan-500 text-white rounded p-2 ${sorting ? `opacity-75 hover:cursor-not-allowed`:`hover:bg-cyan-600`}`} onClick={() => insertionSort(array)}>
-                    Insertion Sort
-                </button>
-                <button className={`bg-cyan-500 text-white rounded p-2 ${sorting ? `opacity-75 hover:cursor-not-allowed`:`hover:bg-cyan-600`}`} onClick={() => randomize(array)}>
-                    Randomize
-                </button>
-            </div>
+                <div className="mt-2 text-center flex gap-1 justify-center">
+                    <span>1ms</span>
+                    <input type="range" min="1" max="1000" defaultValue={delayValue} ref={delayValue} />
+                    <span>1000ms</span>
+                </div>
 
-            <div className="mt-2 text-center flex gap-1 justify-center">
-                <span>1ms</span>
-                <input type="range" min="1" max="1000" defaultValue={delayValue} ref={delayValue} />
-                <span>1000ms</span>
+                <div className="mt-2 text-center flex gap-1 justify-center">
+                    <span>2</span>
+                    <input disabled={sorting} className={`${sorting ? `hover:cursor-not-allowed`:``}`} type="range" min="2" max="100" defaultValue={count} onChange={(e) => setCount(e.target.value)} />
+                    <span>100</span>
+                </div>
             </div>
         </>
     )
